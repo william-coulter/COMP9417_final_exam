cd21a72360b34f3b95af87367b605bf07e128b2d
william-coulter <z5113817@ad.unsw.edu.au>
2021-08-19 11:21:47 +1000

    question 1b

--- code/scripts/solutions.py
+++ code/scripts/solutions.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import log_loss, make_scorer
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -44,8 +45,8 @@ def q1a():
         coef_mat[b,:] = mod.coef_
 
     means = np.mean(coef_mat, axis=0)
-    lower = np.quantile(coef_mat, 0.05, axis=0)
-    upper = np.quantile(coef_mat, 0.95, axis=0)
+    lower = np.quantile(coef_mat, 0.10, axis=0)
+    upper = np.quantile(coef_mat, 0.90, axis=0)
 
     colors = ["red" if lower[i] <= 0 and upper[i] >= 0 else "blue" for i in range(p)]
 
@@ -69,25 +70,47 @@ def q1b():
     C = 1000
     p = Xtrain.shape[1]
 
+    # Train initial model
+    mod = LogisticRegression(penalty="l1", solver="liblinear", C=C).fit(Xtrain, Ytrain)
+    B0 = mod.intercept_[0]
+    coefs_sum = sum(mod.coef_[0])
+    # print(B0)
+    # print(coefs_sum)
+    # print(math.exp(B0 + coefs_sum))
+    # print(mod.coef_[0])
+
+    # Calculate p value for bernoulli distribution
+    # TODO: Probability here is too high
+    prob = math.exp(B0 + coefs_sum) / (1 + math.exp(B0 + coefs_sum))
+    # print(prob)
+
     # Find confidence
     coef_mat = np.zeros(shape=(B,p))
     for b in range(B):
+        # Get X values randomly as before. Here, n = Xtrain.shape[0].
         b_sample = np.random.choice(np.arange(Xtrain.shape[0]), size=Xtrain.shape[0])
         Xtrain_b = Xtrain[b_sample]
-        Ytrain_b = Ytrain[b_sample]
-        mod = LogisticRegression(penalty="l1", solver="liblinear", C=C).fit(Xtrain_b, Ytrain_b)
-        coef_mat[b,:] = mod.coef_
+
+        # The Ytrain_b must come from a bernoulli distribution
+        Ytrain_b = np.random.binomial(n=1, p=prob, size=Xtrain.shape[0])
+        print(Ytrain_b)
+
+        # Train new model
+        mod_b = LogisticRegression(penalty="l1", solver="liblinear", C=C).fit(Xtrain_b, Ytrain_b)
+
+        # store coefficients
+        coef_mat[b,:] = mod_b.coef_
 
     means = np.mean(coef_mat, axis=0)
-    lower = np.quantile(coef_mat, 0.05, axis=0)
-    upper = np.quantile(coef_mat, 0.95, axis=0)
+    lower = np.quantile(coef_mat, 0.10, axis=0)
+    upper = np.quantile(coef_mat, 0.90, axis=0)
 
     colors = ["red" if lower[i] <= 0 and upper[i] >= 0 else "blue" for i in range(p)]
 
     plt.vlines(x=np.arange(1,p+1), ymin=lower, ymax=upper, colors=colors)
     plt.scatter(x=np.arange(1,p+1), y=means, color=colors)
     plt.xlabel("$Features$")
-    plt.savefig("./outputs/NPBootstrap.png", dpi=400)
+    plt.savefig("./outputs/NPParameterisedBootstrap.png", dpi=400)
 
 ### MAIN ###
 
